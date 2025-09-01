a053fd83b0a247d1bb8ed071e410e5e8895a9498
urfanamillatina <urfanamillatina@gmail.com>
2025-09-01 22:11:11 +0700

    updated home.html and app.py files

--- app.py
+++ app.py
@@ -20,5 +20,13 @@ def predict_api():
     print (output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data= [float(x) for x in request.form.values()]
+    final_input = data
+    print(final_input)
+    output = best_model.predict(final_input)[0]
+    return render_template('home.html', prediction_text= "The Loan is{}".format(output))
+
 if __name__ =="__main__":
     app.run(debug=True)
